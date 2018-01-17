84ec28c7e426799db0689d0e402068102f77ae7c
Rio Michelini <riomich98@gmail.com>
2018-01-17 11:13:45 +0000

    Red Blue and Green LED's will now cycle

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,20 @@
 int main() {
 
     DigitalOut red(PTB22);
+    DigitalOut blue(PTB21);
+    DigitalOut green(PTE26);
 
     while(1) {
-      red.write(0);
+      red.write(1);
+      blue.write(0);
+      green.write(1);
       wait(0.5);
+      red.write(0);
+      green.write(1);
+      blue.write(1);
+      wait(0.5);
+      green.write(0);
+      blue.write(1);
       red.write(1);
       wait(0.5);
     }
