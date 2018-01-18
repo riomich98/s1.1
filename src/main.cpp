ea6e1a123e20fdd35f5fc041b518e1c03c415793
Rio Michelini <riomich98@gmail.com>
2018-01-18 16:31:09 +0000

    Added Application Shield LEDS

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,30 @@ void ledOn(DigitalOut led) {
 }
 
 int main() {
-
     DigitalOut red(PTB22);
     DigitalOut blue(PTB21);
     DigitalOut green(PTE26);
+    DigitalOut redShield(D5);
+    DigitalOut blueShield(D8);
+    DigitalOut greenShield(D9);
     // to turn LED off, 1 is used
     // to turn LED on, 0 is used
-
     while(1) {
+      ledOn(blueShield);
+      ledOff(redShield);
       ledOn(blue);
       ledOff(green);
       ledOff(red);
       wait(0.5);
-      ledOn(red);
-      ledOff(blue);
-      wait(0.5);
       ledOn(green);
-      ledOff(red);
+      ledOff(blue);
+      ledOn(greenShield);
+      ledOff(blueShield);
+      wait(0.5);
+      ledOff(green);
+      ledOn(red);
+      ledOff(greenShield);
+      ledOn(redShield);
       wait(0.5);
     }
 }
